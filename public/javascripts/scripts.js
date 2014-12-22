cc349b5f86ccef8045956a8ef8ce50f4bf8672c6
zoosky <zoosky@gmail.com>
2014-12-22 18:29:02 +0100

    remove unused css, add selected css class

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -75,7 +75,7 @@ $(document).on('prismic:rendered', function() {
 	==============================================*/
 	$('a.scrollto').click(function(e){
 		$('html,body').scrollTo(this.hash, this.hash, {gap:{y:-70}});
-		e.preventDefault();
+		//e.preventDefault();
 
 		if ($('.navbar-collapse').hasClass('in')){
 			$('.navbar-collapse').removeClass('in').addClass('collapse');
